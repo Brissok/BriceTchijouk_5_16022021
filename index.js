7745c23cef30fb759a00276191e2017ac30d885d
Brice Tchijouk <63953094+Brissok@users.noreply.github.com>
2021-02-24 10:19:37 +0100

    first styles + header

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
+let products_id = [];
+
 var request = new XMLHttpRequest();
 request.onreadystatechange = function() {
     if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
         var response = JSON.parse(this.responseText);
-        var cardImg = document.getElementById("cardImg");
-        cardImg.innerHTML = "<img src='" + response[0].imageUrl + "' alt='" + response[0].name + "'>";
+        for (let i = 0 ; i < response.lenght ; i++) {
+          let id = response[i]._id;
+          products_id.push(id);
+        }
+        return products_id;
     }
 };
-request.open("GET", "http://localhost:3000/api/cameras/5be1ef211c9d44000030b062");
+request.open("GET", "http://localhost:3000/api/cameras");
 request.send();
 
-  /**
-   * 
-   * @param {string} className
-   * @returns {HTMLElement}
-   */
-  createDivWithClass(className) {
-    let div = document.createElement('div');
-    div.setAttribute("class", className);
-    return div;
-  };
+
+/**
+ * 
+ * @param {objectID} id
+ * @returns {HTMLElement}
+ */
+const createProduct = (id) => {
+  let product = document.createElement('a');
+  product.setAttribute('href', "#");
+  product.setAttribute('class', 'product');
+  product.setAttribute('id', id);
+  return product;
+}
+l
+const createDivImg = (id) => {
+  let imageUrl = ;
+  let div = document.createElement('div');
+  div.setAttribute('class', 'product-img');
+  div.setAttribute('src', imageUrl);
+  div.setAttribute('alt', name);
+}
